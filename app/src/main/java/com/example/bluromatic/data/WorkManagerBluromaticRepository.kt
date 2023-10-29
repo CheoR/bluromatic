a48ab1b17f82b54c0ae5195e2a9b915e50288fea
CheoR <cheo.roman+github@gmail.com>
2023-10-29 17:00:44 -0500

    build(data): create input data object to pass URI to BlueWorker with input data object.

--- app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -8,6 +8,7 @@ import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
+import com.example.bluromatic.getImageUri
 import com.example.bluromatic.workers.BlurWorker
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,6 +16,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
     override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
+    private var imageUri: Uri = context.getImageUri()
     private val workManager = WorkManager.getInstance(context)
 
     /**
@@ -24,6 +26,9 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
     override fun applyBlur(blurLevel: Int) {
         // Create WorkRequest to blur image
         val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
+
+        // input data object
+        blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
 
         // Start work
         workManager.enqueue(blurBuilder.build())
--- app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -4,8 +4,11 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
 import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.KEY_BLUR_LEVEL
+import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -15,6 +18,9 @@ private const val TAG = "BlurWorker"
 
 class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
     override suspend fun doWork(): Result {
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+        val blurLevel = inputData.getInt(KEY_BLUR_LEVEL, 1)
+
         makeStatusNotification(
             applicationContext.resources.getString(R.string.blurring_image),
             applicationContext
@@ -27,15 +33,32 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, 
             // to fix error: return not allowed here
             return@withContext try {
 
+                // throw IllegalArgumentException if first argument evaluates to false
+                require(!resourceUri.isNullOrBlank()) {
+                    val errorMessage =
+                        applicationContext.resources.getString(R.string.invalid_input_uri)
+                    Log.e(TAG, errorMessage)
+                    errorMessage
+                }
+
+                // use ContentResolver object to read contents pointed to by URI
+                val resolver = applicationContext.contentResolver
+
+
                 // Utility function added to emulate slower work.
                 delay(DELAY_TIME_MILLIS)
 
-                val picture = BitmapFactory.decodeResource(
-                    applicationContext.resources,
-                    R.drawable.android_cupcake
-                )
+//                val picture = BitmapFactory.decodeResource(
+//                    applicationContext.resources,
+//                    R.drawable.android_cupcake
+//                )
 
-                val output = blurBitmap(picture, 1)
+                // image source is now the passed in URI
+                val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri))
+                )
+                // val output = blurBitmap(picture, 1)
+                val output = blurBitmap(picture, blurLevel)
 
                 // Write bitmap to temp file
                 val outputUri = writeBitmapToFile(applicationContext, output)
