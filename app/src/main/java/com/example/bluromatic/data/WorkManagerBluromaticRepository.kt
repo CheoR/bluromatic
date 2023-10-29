4dc46a7e570f034b0912b0d22ff52bd59b696c2b
CheoR <cheo.roman+github@gmail.com>
2023-10-29 14:48:16 -0500

    build(data): Create WorkRequest to blur image. Handle interactions with WorkManager and OneTimeWorkRequest.

--- app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -3,21 +3,31 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
+import com.example.bluromatic.workers.BlurWorker
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
     override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
+    private val workManager = WorkManager.getInstance(context)
 
     /**
      * Create WorkRequests to apply blur and save resulting image
      * @param blurLevel amount to blur image
      */
-    override fun applyBlur(blurLevel: Int) {}
+    override fun applyBlur(blurLevel: Int) {
+        // Create WorkRequest to blur image
+        val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
+
+        // Start work
+        workManager.enqueue(blurBuilder.build())
+    }
 
     /**
      * Cancel any ongoing WorkRequests
@@ -39,4 +49,7 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
             )
         return builder.build()
     }
+
+
+
 }
