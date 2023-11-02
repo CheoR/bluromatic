d56744e13e178175271e95b97c99cd2cffa18438
CheoR <cheo.roman+github@gmail.com>
2023-11-02 08:28:30 -0500

    build(data): work policy for chain of work requests

--- app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -3,10 +3,12 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
+import com.example.bluromatic.IMAGE_MANIPULATION_WORK_NAME
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.getImageUri
@@ -38,7 +40,19 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
         // OneTimeWorkRequestBuilder<CleanupWorker>().build().
         // OneTimeWorkRequest class comes from AndroidX Work library while
         // OneTimeWorkRequestBuilder is helper function provided by WorkManager KTX extension.
-        var continuation = workManager.beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
+        // var continuation = workManager
+        //  .beginWith(
+        //      OneTimeWorkRequest.from(CleanupWorker::class.java)
+        //  )
+
+        // to chain work to run at as chunk before starting another chunk of work requests
+        //  provide unique string name. Name entire chain to refer to and query them together
+        var continuation = workManager
+            .beginUniqueWork(
+                IMAGE_MANIPULATION_WORK_NAME,
+                ExistingWorkPolicy.REPLACE,
+                OneTimeWorkRequest.from(CleanupWorker::class.java)
+            )
 
         // Create WorkRequest to blur image
         val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
@@ -82,7 +96,4 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
             )
         return builder.build()
     }
-
-
-
 }
