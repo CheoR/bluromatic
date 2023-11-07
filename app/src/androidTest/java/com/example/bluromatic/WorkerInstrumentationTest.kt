97b20f697a18299acc911b094a4afbb8b84114a3
CheoR <cheo.roman+github@gmail.com>
2023-11-07 08:33:50 -0600

    test(CleanUpWorker): WorkerInstrumentationTest blurWorker_doWork_resultSuccessReturnsUri

--- app/src/androidTest/java/com/example/bluromatic/WorkerInstrumentationTest.kt
+++ app/src/androidTest/java/com/example/bluromatic/WorkerInstrumentationTest.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.work.ListenableWorker
 import androidx.work.testing.TestListenableWorkerBuilder
+import androidx.work.workDataOf
+import com.example.bluromatic.workers.BlurWorker
 import com.example.bluromatic.workers.CleanupWorker
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertTrue
@@ -12,6 +14,8 @@ import org.junit.Test
 
 class WorkerInstrumentationTest {
     private lateinit var context: Context
+    private val mockUriInput: Pair<String, String> =
+        KEY_IMAGE_URI to "android.resource://com.example.bluromatic/drawable/android_cupcake"
 
     @Before
     fun setUp() {
@@ -24,6 +28,23 @@ class WorkerInstrumentationTest {
         runBlocking {
             val result = worker.doWork()
             assertTrue(result is ListenableWorker.Result.Success)
+        }
+    }
+
+    @Test
+    fun blurWorker_doWork_resultSuccessReturnsUri() {
+        val worker = TestListenableWorkerBuilder<BlurWorker>(context)
+            .setInputData(workDataOf(mockUriInput))
+            .build()
+        runBlocking {
+            val result = worker.doWork()
+            val resultUri = result.outputData.getString(KEY_IMAGE_URI)
+            assertTrue(result is ListenableWorker.Result.Success)
+            assertTrue(result.outputData.keyValueMap.containsKey(KEY_IMAGE_URI))
+            assertTrue(
+                resultUri?.startsWith("file:///data/user/0/com.example.bluromatic/files/blur_filter_outputs/blur-filter-output-")
+                    ?: false
+            )
         }
     }
 
